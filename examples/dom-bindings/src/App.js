402fdc7f1c4739b4bbffa44eba39fb3c2b2883d5
Benji Koltai <koltaibenji@gmail.com>
2018-01-30 13:54:51 -0800

    Demonstrate broken unmounted when used in DOM

--- examples/dom-bindings/src/App.js
+++ examples/dom-bindings/src/App.js
@@ -3,30 +3,52 @@ import { Document } from '@react-pdf/dom';
 import { Page } from '@react-pdf/core';
 import Fractal from './Fractal';
 
-const App = () => (
-  <div className="app">
-    <div className="header">
-      <h2>React PDF DOM bindings</h2>
-    </div>
-    <Document
-      title="Fractals"
-      author="John Doe"
-      subject="Rendering fractals with react-pdf"
-      keywords={['react', 'pdf', 'fractals']}
-    >
-      <Page size="A4">
-        <Fractal steps={18} />
-      </Page>
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      showDocument: true,
+    };
 
-      <Page orientation="landscape" size="A4">
-        <Fractal steps={14} />
-      </Page>
+    this.handleToggleShow = this.handleToggleShow.bind(this);
+  }
 
-      <Page size="B4">
-        <Fractal steps={10} />
-      </Page>
-    </Document>
-  </div>
-);
+  handleToggleShow() {
+    this.setState({ showDocument: !this.state.showDocument });
+  }
+
+  render() {
+    const { showDocument } = this.state;
+    return (
+      <div className="app">
+        <div className="header">
+          <h2>React PDF DOM bindings</h2>
+        </div>
+
+        <button onClick={this.handleToggleShow}>Toggle show</button>
+        {showDocument && (
+          <Document
+            title="Fractals"
+            author="John Doe"
+            subject="Rendering fractals with react-pdf"
+            keywords={['react', 'pdf', 'fractals']}
+          >
+            <Page size="A4">
+              <Fractal steps={18} />
+            </Page>
+
+            <Page orientation="landscape" size="A4">
+              <Fractal steps={14} />
+            </Page>
+
+            <Page size="B4">
+              <Fractal steps={10} />
+            </Page>
+          </Document>
+        )}
+      </div>
+    );
+  }
+}
 
 export default App;
